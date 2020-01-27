3034807842647266f54c9a3585a183b3fe2c8e0b
Chuck Weinberger <chuckhw@sbcglobal.net>
2020-01-26 19:12:20 -0800

    Starting to develop object inheritance a little better

--- Formal_Dinner_Assigner.py
+++ Formal_Dinner_Assigner.py
@@ -1,12 +1,13 @@
 import random
 import math
-import sys
 
 ####################
 # GLOBAL CONSTANTS #
 ####################
 
-OUTPUTFILE = "Dinner Assignments.csv"
+INPUT_FILE = "sample_data.csv"
+
+OUTPUT_FILE = "Dinner Assignments.csv"
 
 WORK_TYPES = ("Kitchen_crew", "Waiter", "Diner")
 
@@ -23,20 +24,50 @@ NUMBER_OF_WAITERS_AT_A_TABLE = 1
 # Object Definitions #
 ######################
 
-class Waiter:
+class Person(object):
+
+    def __init__(self, lname, fname):
+        self.lname = lname
+        self.fname = fname
+
+class Attendant(Person):
+
+    def __init__(self, lname, fname):
+        super().__init__(lname, fname)
+        self.assigned = False
+        self.assignable = True
+
+class Student(Attendant):
+
+    all = []
+
+    def __init__(self, lname, fname):
+        super().__init__(lname, fname)
+        self.all.append(self)
+
+    def __str__(self):
+        if self.type_of_assignment == "Kitchen_crew":
+            last_s = "Kitchen"
+        elif self.type_of_assignment == "Waiter":
+            last_s = "W %s" % (self.waiter.table.number)
+        else:
+            last_s = "%s" % (self.diner.table.number)
+        return "%s, %s, %s\n" % (self.lname, self.fname, last_s)
+
+class Waiter():
     all = []
 
     def __init__(self, parent, table_number):
         self.super = parent
         parent.assigned = True
+        parent.waiter = self
         parent.type_of_assignment = "Waiter"
-        parent.table_number = table_number
         self.all.append(self)
 
     def __str__(self):
-        return "%s, %s, W%s" % (self.super.lname, self.super.fname, self.super.table_number)
+        return "%s, %s, W%s" % (self.super.lname, self.super.fname, self.table.number)
 
-class Kitchen_crew:
+class Kitchen_crew(Student):
     all = []
 
     def __init__(self, parent, number):
@@ -48,51 +79,38 @@ class Kitchen_crew:
     def __str__(self):
         return "%s, %s, Kitchen" % (self.super.lname, self.super.fname)
 
-class Diner:
+class Diner(Student):
     all = []
 
     def __init__(self, parent, table_number):
         self.super = parent
         parent.assigned = True
-        parent.table_number = table_number
+        parent.diner = self
         parent.type_of_assignment = "Diner"
         self.all.append(self)
 
     def __str__(self):
-        return "%s, %s, %s" % (self.super.lname, self.super.fname, self.super.table_number)
-
-class Student:
-    all = []
-
-    def __init__(self, lname, fname):
-        self.lname = lname
-        self.fname = fname
-        self.all.append(self)
-        self.assigned = False
-
-    def __str__(self):
-        if self.type_of_assignment == "Kitchen_crew":
-            last_s = "Kitchen"
-        elif self.type_of_assignment == "Waiter":
-            last_s = "W %s" % (self.table_number)
-        else:
-            last_s = "%s" % (self.table_number)
-        return "%s, %s, %s\n" % (self.lname, self.fname, last_s)
+        return "%s, %s, %s" % (self.super.lname, self.super.fname, self.table.number)
 
 class Table:
     all = []
 
-    def __init__(self, number, diner):
+    def __init__(self, number):
         self.number = number
         self.all.append(self)
         self.full = False
-        self.diners = [diner]
+        self.diners = []
+        self.waiters = []
 
     def add_diner(self, diner, number_needed):
         self.diners.append(diner)
         diner.table = self
         if len(self.diners) == number_needed:
             self.full = True
+
+    def add_waiter(self, waiter):
+        self.waiters.append(waiter)
+        waiter.table = self
 
 #############################
 # local function defintions #
@@ -127,41 +145,59 @@ def calc_number_of_tables_needed():
 
     return math.ceil((len(Student.all) - NUMBER_IN_KITCHEN_STAFF) / (MAX_NUMBER_SITTING_AT_A_TABLE + NUMBER_OF_WAITERS_AT_A_TABLE))
 
-def assign_work(*params): # work_type, work_force, number_needed, maybe table_number
-    work_type = params[0]
-    work_force = params[1]
-    number_needed = params[2]
-    unassigned = list(filter(lambda obj: obj.assigned == False, work_force))
+#Takes in the type of work, the class representing the workers (e.g., Student, but in the future maybe also Teacher)
+# and the number of that class needed to perform the work.
+def assign_work(work_type, work_force, number_needed):
+
+    #get a list of Student objects that aren't already assigned to other work
+    unassigned = list(filter(lambda obj: obj.assigned == False and obj.assignable == True, work_force))
 
     i = 1
-    while i <= number_needed and len(unassigned) > 0:
+    while i <= number_needed and len(unassigned) > 0: #assign the number_needed
+
+        # choose a number at random from the list of available students
         available_pool = len(unassigned)
-        # choose a number at random from a set equal to the indecises of the list of available students
         rand = random.randint(0, (available_pool - 1))
+
+        # remove assignee from the unassigned list so that we don't double assign the object
         assignee = unassigned.pop(rand)
 
-        #get the correct Class type using a string that matches the Class name
+        #get the correct Class-type using a string that matches the Class name
         cls = str_to_class(work_type)
 
-        #assigning diners to tables is a special case because we need to create a Table object for the first diner, but
-        #after that we only need to add the diner to the existing table
-        if work_type != "Diner":
-            cls(assignee, i)
-        else:
-            table_number = params[3]
-            diner = Diner(assignee, table_number)
-            if i == 1:  # this is the first diner for this table, so create a new Table object
-                Table(table_number, diner)
-            else:  # we have already created this table, so just add diner to existing table
-                Table.all[table_number - 1].add_diner(diner, number_needed)
+        worker = cls(assignee, i) #create the correct type of worker object
+        if work_type == "Waiter": #if the worker is a waiter add them to the correct table_number
+            table = worker.table= Table(i)
+            table.add_waiter(worker)
+
         i += 1
+
+def assign_dinner_tables(number_of_tables_needed):
+
+    # get a list of the attendants who do not have a work assignment
+    needing_a_table = list(filter(lambda obj: obj.assigned == False, Student.all))
+
+    for table_number in range(1, number_of_tables_needed + 1):
+
+        for table_position in range(1, MAX_NUMBER_SITTING_AT_A_TABLE + 1):
+
+            #handle the case where we don't have enough students to fill the last table
+            if len(needing_a_table) <1:
+                break
+
+            # an index for a diner chosen at random
+            rand = random.randint(0, (len(needing_a_table) - 1))
+            diner = needing_a_table.pop(rand)
+            diner = Diner(diner, table_number)
+            table = Table.all[table_number - 1] #note, the Table obj was created when we assigned waiters
+            table.add_diner(diner, MAX_NUMBER_SITTING_AT_A_TABLE)
 
 #################
 # Main Body     #
 #################
 
 #read all students from a csv file and create a Student object for each one of them
-read_students_file("sample_data.csv")
+read_students_file(INPUT_FILE)
 
 #determine how many tables we need given the capacity of each table and the total number
 #of people that we need to seat for dinner
@@ -171,9 +207,8 @@ number_of_tables_needed = calc_number_of_tables_needed()
 assign_work(WORK_TYPES[0], Student.all, NUMBER_IN_KITCHEN_STAFF)
 assign_work(WORK_TYPES[1], Student.all, number_of_tables_needed * NUMBER_OF_WAITERS_AT_A_TABLE)
 
-#assign tables to the non-workers,
-for x in range(number_of_tables_needed):
-    assign_work(WORK_TYPES[2], Student.all, MAX_NUMBER_SITTING_AT_A_TABLE, x)
+#assign the non-workers to a dinner table
+assign_dinner_tables(number_of_tables_needed)
 
 #print status of each student to an output file
-write_all_of_a_class(Student, OUTPUTFILE)
+write_all_of_a_class(Student, OUTPUT_FILE)
